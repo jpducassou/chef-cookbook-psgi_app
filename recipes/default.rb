6ad6329a52602733a31819432a0a5738681c51e4
Jean Pierre Ducassou <jpducassou@gmail.com>
2015-01-01 23:26:44 -0200

    Fixed log messages

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,18 +2,18 @@
 # ============================================================================
 # Checks
 # ============================================================================
-Chef::Log.fatal "perl_version not set for application" unless node['application']['perl_version']
+Chef::Log.fatal "perl_version not set for application" unless node['psgi_app']['perl_version']
 
 # ============================================================================
 # perlbrew
 # ============================================================================
 # node['perlbrew']['self_upgrade'] = false
 node.default['perlbrew']['self_upgrade'] = false
-Chef::Log.info "Installing perlbrew"
+Chef::Log.info 'Installing perlbrew ...'
 include_recipe 'perlbrew'
 
-Chef::Log.info "Brewing perl: #{node['application']['[perl_version']}"
-perlbrew_perl node['application']['perl_version'] do
+Chef::Log.info "Brewing perl: \"#{node['psgi_app']['perl_version']}\" ..."
+perlbrew_perl node['psgi_app']['perl_version'] do
 	action :install
 end
 
@@ -36,17 +36,15 @@ end
 # ============================================================================
 # Carton
 # ============================================================================
-
 #include_recipe 'carton'
-#
 
-#
-#
-#carton_app "cgi_app" do
-#  perlbrew node['cgi_app']['perl_version']
-#  command "starman -p #{node['cgi_app']['port']} app.psgi"
-#  cwd node['cgi_app']['deploy_dir']
-#  user node['cgi_app']['user']
+#carton_app "psgi_app" do
+#  perlbrew node['psgi_app']['perl_version']
+#  command "starman -p #{node['psgi_app']['port']} app.psgi"
+#  cwd   node['cgi_app']['deploy_dir']
+#  user  node['cgi_app']['user']
 #  group node['cgi_app']['group']
 #end
+#
+
 #
