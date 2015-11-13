15986ab06c7798a54932da20df69c4adcfb7a213
Jean Pierre Ducassou <jpducassou@gmail.com>
2015-11-13 09:22:41 -0300

    Fixed a couple of bugs.

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,7 +7,6 @@ Chef::Log.fatal "perl_version not set for application" unless node['psgi_app']['
 # ============================================================================
 # perlbrew
 # ============================================================================
-# node['perlbrew']['self_upgrade'] = false
 node.default['perlbrew']['self_upgrade'] = false
 Chef::Log.info 'Installing perlbrew ...'
 include_recipe 'perlbrew'
@@ -20,13 +19,19 @@ end
 # ============================================================================
 # Required distro packages
 # ============================================================================
-include_recipe 'apt'
-
 required_packages = node['psgi_app']['required_packages']
-required_packages.each do |pkg|
-	package pkg do
-		action :install
+
+if required_packages then
+
+	include_recipe 'apt'
+
+	required_packages.each do |pkg|
+		package pkg do
+			action :install
+		end
 	end
+else
+	Chef::Log.info "apt: no packages set."
 end
 
 # ============================================================================
@@ -44,9 +49,9 @@ include_recipe 'carton'
 carton_app "psgi_app" do
 	perlbrew node['psgi_app']['perl_version']
 	# command "starman -p #{node['psgi_app']['port']} velia.psgi"
-	deploy_dir node['psgi_app']['deploy_dir']
 	user       node['psgi_app']['user']
 	group      node['psgi_app']['group']
+	cwd        node['psgi_app']['deploy_dir']
 end
 
 #carton_app "psgi_app" do
